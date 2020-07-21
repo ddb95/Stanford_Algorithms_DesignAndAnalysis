7967d101d03cca1d8887f9c5daa1a9342acd83dd
ddb95 <debadatta1995@outlook.com>
2020-07-22 00:11:04 +0530

    -- linked list append and get position and insert

--- udacity/linkedList.py
+++ udacity/linkedList.py
@@ -29,18 +29,36 @@ class LinkedList(object):
         else:
             self.head = new_element
 
-    # def get_position(self, position):
-    #     """Get an element from a particular position.
-    #     Assume the first position is "1".
-    #     Return "None" if position is not in the list."""
-    #     return None
+    def get_position(self, position):
+        """Get an element from a particular position.
+        Assume the first position is "1".
+        Return "None" if position is not in the list."""
+        counter = 0
+        current = self.head
+        while current:
+            counter = counter + 1
+            if counter == position:
+                if current.value != None:
+                    return current
+            else:
+                current = current.next
 
-    # def insert(self, new_element, position):
-    #     """Insert a new node at the given position.
-    #     Assume the first position is "1".
-    #     Inserting at position 3 means between
-    #     the 2nd and 3rd elements."""
-    #     pass
+    def insert(self, new_element, position):
+        """Insert a new node at the given position.
+        Assume the first position is "1".
+        Inserting at position 3 means between
+        the 2nd and 3rd elements."""
+        counter = 0
+        current = self.head
+        while current:
+            counter = counter + 1
+            if counter == position-1:
+                previousValue = current
+                nextValue = current.next
+                new_element.next = nextValue
+                previousValue.next = new_element
+            else:
+                current = current.next
 
     # def delete(self, value):
     #     """Delete the first node with a given value."""
@@ -53,6 +71,8 @@ e1 = Element(1)
 e2 = Element(2)
 e3 = Element(3)
 e4 = Element(4)
+e5 = Element(5)
+e6 = Element(6)
 
 # Start setting up a LinkedList
 ll = LinkedList(e1)
@@ -61,14 +81,22 @@ ll.append(e3)
 
 # Test get_position
 # Should print 3
-print(ll.head.next.next.value)
+# print(ll.head.next.next.value)
 # # Should also print 3
+# print(ll.get_position(3).value)
 # print(ll.get_position(3).value)
 
 # Test insert
-# ll.insert(e4, 3)
+ll.insert(e4, 3)
+ll.insert(e5, 2)
+ll.insert(e6, 6)
 # # Should print 4 now
-# print(ll.get_position(3).value)
+print(ll.get_position(1).value)
+print(ll.get_position(2).value)
+print(ll.get_position(3).value)
+print(ll.get_position(4).value)
+print(ll.get_position(5).value)
+print(ll.get_position(6).value)
 
 # Test delete
 # ll.delete(1)
