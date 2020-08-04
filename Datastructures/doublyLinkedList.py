2a69a1296ddea216d9312542d02756a1023f2d10
ddb95 <debadatta1995@outlook.com>
2020-08-04 23:53:23 +0530

    -- full of bugs in doubly linked list

--- Datastructures/doublyLinkedList.py
+++ Datastructures/doublyLinkedList.py
@@ -1,10 +1,30 @@
-class Node(object):
-    def __init__(self, element, prev, next):
+class Element(object):
+    __slots__ = 'element', 'prev', 'next'
+
+    def __init__(self, element=None, prevNode=None, nextNode=None):
         self.element = element
-        self.prev = prev
-        self.next = next
+        self.prev = prevNode
+        self.next = nextNode
 
 
 class DoublyLinkedList(object):
-    def __init__(self):
-        pass
+    def __init__(self, header=None, trailer=None):
+        self.header = header
+        self.trailer = trailer
+
+    def append(self, value):
+        current = self.header
+        if self.header and self.trailer:
+            return 'sa'
+        else:
+            self.header.next = value
+            self.trailer.prev = value
+
+
+if __name__ == "__main__":
+    e1 = Element(1)
+    e2 = Element(2)
+    e3 = Element(3)
+    ll = DoublyLinkedList()
+    ll.append(e1)
+    ll.append(e2)
